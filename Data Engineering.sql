e965d3767fa71ad09809d92522f410dc153b836a
unknown <koifish.analytics@gmail.com>
2025-01-05 16:43:49 -0800

    work to code

---
+++ Data Engineering.sql
@@ -0,0 +1,54 @@
+DO $$ 
+DECLARE 
+    r RECORD;
+BEGIN 
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') 
+    LOOP 
+        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+    END LOOP; 
+END $$;
+
+CREATE TABLE departments (
+    dept_no INT PRIMARY KEY,
+    dept_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE titles (
+    emp_title_id VARCHAR(50) PRIMARY KEY,
+    title VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+    emp_title_id VARCHAR(50) NOT NULL,
+	birth_date Date NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date Date NOT NULL,
+	Foreign Key (emp_title_id) References titles(emp_title_id)
+);
+
+CREATE TABLE dept_emp (
+    dept_no INT NOT NULL,
+    emp_no INT NOT NULL,
+	Primary Key (dept_no, emp_no),
+	Foreign Key (dept_no) References departments(dept_no),
+	Foreign Key (emp_no) References employees(emp_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no INT NOT NULL,
+    emp_no INT NOT NULL,
+	Primary Key (dept_no, emp_no),
+	Foreign Key (dept_no) References departments(dept_no),
+	Foreign Key (emp_no) References employees(emp_no)
+);
+
+
+CREATE TABLE salaries (
+    emp_no INT PRIMARY KEY,
+    salary Decimal(10,2) NOT NULL,
+	Foreign Key (emp_no) References employees(emp_no)
+);
+
